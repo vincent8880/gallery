e9ee97c4386af0a635e758c8c8f5ee83a4e0838c
vincent <ododovincent54@gmail.com>
2019-07-19 12:08:58 +0300

    added boot4strap to my project

---
+++ phot4/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Phot4Config(AppConfig):
+    name = 'phot4'
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/vincent/Desktop/The-gallery-project/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
