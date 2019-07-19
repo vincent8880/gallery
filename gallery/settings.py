0e4bf7c80b6ee9bf884af413d94d93158ca3fc66
vincent <ododovincent54@gmail.com>
2019-07-19 12:12:00 +0300

    addedboot4st4p t4o my7 installed apps in settings.py

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'phot4.apps.Phot4Config',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
