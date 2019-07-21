69347a8fc4a49ac28d568418cdd0d8f281c60e8a
vincent <ododovincent54@gmail.com>
2019-07-21 19:09:42 +0300

    changed my default url page

--- phot4/urls.py
+++ phot4/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 
-urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+urlpatterns = [
+    url('^$',views.index, name = 'home'),
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single_image, name='art'),
+    url(r'^location/(?P<location>\d+)', views.location_filter, name='location_filter'),
+    url(r'^search', views.search, name='search_images')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
