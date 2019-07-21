523242c5e499c1458a62add2ac1a3b68312e2293
vincent <ododovincent54@gmail.com>
2019-07-21 19:02:23 +0300

    updat4ed my7 views.py file

--- phot4/views.py
+++ phot4/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Image, Location, Category
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'index.html')
+def index(request):
+    title = 'Home'
+    images = Image.get_all_images()
+    locations = Location.objects.all()
+    return render(request, 'index.html', {'title':title, 'images':images, 'locations':locations})
+
+def single_image(request, category_name, image_id):
+    # print(image_category)
+    locations = Location.objects.all()
+
+    image = Image.get_image_by_id(image_id)
+    # Get category name
+    # print(category_name)
+    image_category = Image.objects.filter(category__photo_category = category_name)
+    title = f'{category_name}'
+    return render(request,'single_image.html',{'title':title, 'image':image, 'image_category':image_category, 'locations':locations})
+
+def location_filter(request, location):
+    locations = Location.objects.all()
+    images = Image.filter_by_location(location)
+    title = f'{location} Photos'
+    return render(request, 'location.html', {'title':title, 'images':images, 'locations':locations})
+
+def search(request):
+    locations = Location.objects.all()
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        images_found = Image.search_image(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html',{'message':message, 'images':images_found, 'locations':locations})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{'message':message, 'locations':locations})
